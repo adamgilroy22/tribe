53c592b81c80c3b4a4d1fc0e64c900a6b928ec06
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-03 18:03:45 +0000

    Creat Profile model class

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Profile model
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=30, blank=True, null=True)
+    bio = models.TextField(max_length=150, blank=True, null=True)
