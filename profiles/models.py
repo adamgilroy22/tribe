b4ef450bcd258fefc101fe8955b3cfe7edb7102c
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-07 13:13:29 +0000

    Add cloudinary to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class Profile(models.Model):
@@ -9,3 +10,5 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=30, blank=True, null=True)
     bio = models.TextField(max_length=150, blank=True, null=True)
+    profile_pic = CloudinaryField('profile_pic', default='placeholder')
+    bg_pic = CloudinaryField('bg_pic', default='placeholder')
