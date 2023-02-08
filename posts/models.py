c1b3c9c1a59626ef833e6c072f90b2d252abcca1
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-08 00:18:31 +0000

    Create Post model class

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Post model
+    """
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=200)
+    posted_on = models.DateTimeField(auto_now_add=True)
+    is_private = models.BooleanField(default=False, editable=True)
+    is_flagged = models.BooleanField(default=False, editable=True)
