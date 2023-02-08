d86aba3f14825fe59be624acc515ce99397a4fc1
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-08 00:25:29 +0000

    Create Comment model class

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Comment model
+    """
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=200)
+    posted_on = models.DateTimeField(auto_now_add=True)
+    post = models.OneToOneField('posts.Post', on_delete=models.CASCADE)
+    is_flagged = models.BooleanField(default=False, editable=True)
