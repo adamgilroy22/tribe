0dc71bb2e0998a7c86856f35cd154ed7af10ea06
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-11 11:44:19 +0000

    Link feed to nav menu

--- feed/urls.py
+++ feed/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import PostListView
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post-list'),
+    path('', PostListView.as_view(), name='feed'),
 ]
