3cbd5868055e5821bc0c159171d6277da811514c
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-15 16:31:53 +0000

    Add profiles to urls.py

--- tribe/urls.py
+++ tribe/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('feed/', include('posts.urls')),
+    path('profile/', include('profiles.urls')),
 ]
