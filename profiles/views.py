164dccb96c24727fbd7c4d8dbaa5d3c97fd0d9ab
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-15 15:58:31 +0000

    Create ProfileView

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Profile
+from posts.models import Post
 
-# Create your views here.
+
+class ProfileView(View):
+    def get(self, request, pk, *args, **kwargs):
+        profile = Profile.objects.get(pk=pk)
+        user = profile.user
+        posts = Post.object.filter(author=user).order_by('-posted_on')
+
+        context = {
+            'user': user,
+            'profile': profile,
+            'posts': posts,
+        }
+
+        return render(request, 'profile.html', context)
