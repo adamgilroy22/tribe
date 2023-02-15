f771f226841209af3d210f3b55a838d121e3e51d
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-15 14:45:51 +0000

    Add related_name to user in Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -7,7 +7,9 @@ class Profile(models.Model):
     """
     Profile model
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User,
+                                related_name='profile',
+                                on_delete=models.CASCADE)
     display_name = models.CharField(max_length=30, blank=True, null=True)
     bio = models.TextField(max_length=150, blank=True, null=True)
     profile_pic = CloudinaryField('profile_pic', default='placeholder')
