ae979523aeec5df4099c774d398e99678300c9f2
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-15 19:27:00 +0000

    Add ProfileEditView

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.urls import reverse_lazy
 from .models import Profile
 from posts.models import Post
 
@@ -17,3 +20,13 @@ class ProfileView(View):
         }
 
         return render(request, 'profile.html', context)
+
+
+class ProfileEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Profile
+    fields = ['display_name', 'bio', 'profile_pic', 'bg_pic']
+    template_name = 'profile_edit.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('profile', kwargs={'pk': pk})
