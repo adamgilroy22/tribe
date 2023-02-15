d4628a065f6a776fa087dc9dfbb1b203c6d05dac
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-15 19:47:37 +0000

    Create profile_edit page

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProfileView
+from .views import ProfileView, ProfileEditView
 
 urlpatterns = [
     path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
+    path('profile/edit/<int:pk>', ProfileEditView.as_view(), name='profile-edit'),
 ]
