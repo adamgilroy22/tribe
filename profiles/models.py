1dc25e20aab1170e3a4878a57e45180b54fb30c0
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-15 17:39:33 +0000

    Rename default profile picture file

--- profiles/models.py
+++ profiles/models.py
@@ -13,6 +13,6 @@ class Profile(models.Model):
     display_name = models.CharField(max_length=30, blank=True, null=True)
     bio = models.TextField(max_length=150, blank=True, null=True)
     profile_pic = CloudinaryField('profile_pic',
-                                  default='/static/img/default-profile-image.png')
+                                  default='/static/img/default-profile-pic.png')
     bg_pic = CloudinaryField('bg_pic', default='placeholder')
     is_suspended = models.BooleanField(default=False, editable=True)
