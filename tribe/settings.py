80859fbf3f03d566706942237efacffa843b3f9d
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-16 12:28:14 +0000

    Convert DEBUG to environment variable

--- tribe/settings.py
+++ tribe/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['tribe.herokuapp.com', 'localhost']
 
