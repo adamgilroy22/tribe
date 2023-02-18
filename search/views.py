53d66d4406a8ad3df2a4cbbc2717a4d1dc92f255
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-18 08:58:13 +0000

    Add Search view

--- search/views.py
+++ search/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views import View
+from django.db.models import Q
+from posts.models import Post
 
-# Create your views here.
+
+class Search(View):
+    def get(self, request, *args, **kwargs):
+        query = self.request.GET.get('query')
+
+        profile_list = Profile.objects.filter(
+            Q(user__username__icontains=query)
+        )
+
+        post_list = Post.objects.filter(
+            Q(post__content__icontains=query)
+        )
+
+        context = {
+            'profile-list': profile_list,
+            'post_list': post_list,
+        }
+
+        return render(request, 'search.html', context)
