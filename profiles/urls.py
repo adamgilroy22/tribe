8abae1e99485f5c35267212a7a096ce4e55f11a9
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-23 13:08:54 +0000

    Add followers_list.html

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProfileView, ProfileEditView, AddFollower, RemoveFollower
+from .views import ProfileView, ProfileEditView, AddFollower, RemoveFollower, FollowersList
 
 urlpatterns = [
     path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
     path('profile/edit/<int:pk>', ProfileEditView.as_view(), name='profile-edit'),
+    path('profile/<int:pk>/followers', FollowersList.as_view(), name='followers-list'),
     path('profile/<int:pk>/followers/add', AddFollower.as_view(), name='add-follower'),
     path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove-follower'),
 ]
