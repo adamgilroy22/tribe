01762dfa1d803d5a4e3ee6076ad4b4854275580a
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-25 16:21:35 +0000

    Create notifications.js and add showNotifications function

---
+++ static/js/notifications.js
@@ -0,0 +1,9 @@
+function showNotifications() {
+    const container = document.getElementById('notification-container');
+
+    if (container.classList.contains('d-none')) {
+        container.classList.remove('d-none');
+    } else {
+        container.classList.add('d-none');
+    };
+}
