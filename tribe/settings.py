bc5a357ff52a605ebaefc1bb03289dcc209e823f
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-27 19:03:28 +0000

    Add MESSAGE_TAGS to settings.py

--- tribe/settings.py
+++ tribe/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -107,6 +108,14 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/feed'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 WSGI_APPLICATION = 'tribe.wsgi.application'
 
