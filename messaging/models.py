98312221942196c3b40467293a56461a0675deb0
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-28 15:18:49 +0000

    Create models for messaging

--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MessageThread(models.Model):
+    """
+    Conversations between users
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+
+
+class MessageModel(models.Model):
+    """
+    Messages inside conversations between users
+    """
+    thread = models.ForeignKey('MessageThread', related_name='+', on_delete=models.CASCADE, blank=True, null=True)
+    message_sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+    message_receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+    message_content = models.CharField(max_length=500)
+    sent_at = models.DateTimeField(default=timezone.now)
+    is_read = models.BooleanField(default=False)
