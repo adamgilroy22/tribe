cb89b09b43320d6b350b79038f900cfa8dd4c20d
Adam Gilroy <adamgilroy22@gmail.com>
2023-02-28 15:25:43 +0000

    Create MessageThreads view

--- messaging/views.py
+++ messaging/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from django.db.models import Q
+from .models import MessageThread, MessageModel
 
-# Create your views here.
+
+class MessageThreads(View):
+    """
+    View message threads
+    """
+    def get(self, request, *args, **kwargs):
+        threads = MessageThread.objects.filter(Q(user=request.user) | Q(receiver=request.user))
+
+        context = {
+            'threads': threads,
+        }
+
+        return render(request, 'inbox.html', context)
