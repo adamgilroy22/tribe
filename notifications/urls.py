02f4e94f545ed5545ad8dd624f301676514bae1d
Adam Gilroy <adamgilroy22@gmail.com>
2023-03-01 02:00:06 +0000

    Create url path for ThreadNotification

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import PostNotification, FollowNotification, RemoveNotification
+from .views import PostNotification, FollowNotification, ThreadNotification, RemoveNotification
 
 urlpatterns = [
     path('notification/<int:notification_pk>/post/<int:post_pk>', PostNotification.as_view(), name='post-notification'),
     path('notification/<int:notification_pk>/profile/<int:profile_pk>', FollowNotification.as_view(), name='follow-notification'),
+    path('notification/<int:notification_pk>/thread/<int:object_pk>', ThreadNotification.as_view(), name='thread-notification'),
     path('notification/delete/<int:notification_pk>', RemoveNotification.as_view(), name='notification-delete'),
 ]
