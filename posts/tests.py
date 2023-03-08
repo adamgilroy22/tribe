fe7ae084abea8399cfd7f5531a39e1055ff0e104
Adam Gilroy <adamgilroy22@gmail.com>
2023-03-08 17:41:25 +0000

    Create post form test

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .forms import PostForm
 
-# Create your tests here.
+
+class TestPostForm(TestCase):
+    def test_post_content_is_required(self):
+        form = PostForm({'content': ""})
+        self.assertFalse(form.is_valid())
+        self.assertIn('content', form.errors.keys())
+        self.assertEqual(form.errors['content'][0], 'This field is required.')
