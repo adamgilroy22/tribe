cdc882bf14475b9ae1d5ea3efca8c269ff6e3574
Adam Gilroy <adamgilroy22@gmail.com>
2023-03-08 17:41:18 +0000

    Create comment form test

--- comments/tests.py
+++ comments/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .forms import CommentForm
 
-# Create your tests here.
+
+class TestCommentForm(TestCase):
+    def test_comment_is_required(self):
+        form = CommentForm({'comment': ""})
+        self.assertFalse(form.is_valid())
+        self.assertIn('comment', form.errors.keys())
+        self.assertEqual(form.errors['comment'][0], 'This field is required.')
