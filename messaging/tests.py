2204292033120d7da30f1d901a8776e9fe225020
Adam Gilroy <adamgilroy22@gmail.com>
2023-03-08 17:41:48 +0000

    Create messageform and threadform tests

--- messaging/tests.py
+++ messaging/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .forms import ThreadForm, MessageForm
 
-# Create your tests here.
+
+class TestThreadForm(TestCase):
+    def test_username_is_required(self):
+        form = ThreadForm({'username': ""})
+        self.assertFalse(form.is_valid())
+        self.assertIn('username', form.errors.keys())
+        self.assertEqual(form.errors['username'][0], 'This field is required.')
+
+
+class TestMessageForm(TestCase):
+    def test_message_is_required(self):
+        form = MessageForm({'username': ""})
+        self.assertFalse(form.is_valid())
+        self.assertIn('message', form.errors.keys())
+        self.assertEqual(form.errors['message'][0], 'This field is required.')
