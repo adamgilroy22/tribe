0ac4d50f1584c0f45369bf0599b92e191c27e640
Adam Gilroy <adamgilroy22@gmail.com>
2023-03-09 12:12:23 +0000

    Fix line too long error in notification_tags.py

--- notifications/templatetags/notification_tags.py
+++ notifications/templatetags/notification_tags.py
@@ -7,5 +7,6 @@ register = template.Library()
 @register.inclusion_tag('notifications.html', takes_context=True)
 def show_notifications(context):
     request_user = context['request'].user
-    notifications = Notification.objects.filter(to_user=request_user).exclude(user_has_seen=True).order_by('-date')
+    notifications = Notification.objects.filter(
+        to_user=request_user).exclude(user_has_seen=True).order_by('-date')
     return {'notifications': notifications}
